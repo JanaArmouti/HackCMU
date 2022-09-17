00895b01b0a271b90695c56faf1a8840a263d835
JanaArmouti <94333888+JanaArmouti@users.noreply.github.com>
2022-09-16 23:58:06 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,25 +5,31 @@ function isArticle(currentWebsite){
     
     if (currentWebsite.startsWith("https://www.bbc.com/news")){
         if (typeof currentWebsite[nameLen - 1] == Number){
-            //return check if reached end of site
+            window.onscroll = function(ev) {
+                if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
+                    alert("you're at the bottom of the page");
+                }
+            };
         }
     }
 
     if (currentWebsite.startsWith("https://www.cnn.com")){
         if (currentWebsite.endsWith("/index.html")){
-            //return check if reached end of site
+            window.onscroll = function(ev) {
+                if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
+                    alert("you're at the bottom of the page");
+                }
+            };
         }
     }
 
     if (currentWebsite.startsWith("https://www.nbcnews.com")){
         if (typeof currentWebsite[nameLen - 1] == Number){
-            //return check if reached end of site
+            window.onscroll = function(ev) {
+                if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
+                    alert("you're at the bottom of the page");
+                }
+            };
         }
     }
 }
-
-window.onscroll = function(ev) {
-    if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
-        alert("you're at the bottom of the page");
-    }
-};
