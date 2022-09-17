dd8179cee162dc43cede071b38fecf1c8d4c484a
JanaArmouti <94333888+JanaArmouti@users.noreply.github.com>
2022-09-16 23:47:31 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,27 @@
 var currentWebsite = window.location.href
 
+function isArticle(currentWebsite){
+    var nameLen = currentWebsite.length
+    
+    if (currentWebsite.startsWith("https://www.bbc.com/news")){
+        if (typeof currentWebsite[nameLen - 1] == Number){
+            //return check if reached end of site
+        }
+    }
+
+    if (currentWebsite.startsWith("https://www.cnn.com")){
+        if (currentWebsite.endsWith("/index.html")){
+            //return check if reached end of site
+        }
+    }
+
+    if (currentWebsite.startsWith("https://www.nbcnews.com")){
+        if (typeof currentWebsite[nameLen - 1] == Number){
+            //return check if reached end of site
+        }
+    }
+}
+
 window.onscroll = function(ev) {
     if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
         alert("you're at the bottom of the page");
