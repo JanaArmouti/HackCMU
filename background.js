714b68e3455440bace92ba309e48eaf4dabe2e10
JanaArmouti <94333888+JanaArmouti@users.noreply.github.com>
2022-09-16 22:49:51 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var currentWebsite = window.location.href
+
 window.onscroll = function(ev) {
     if ((window.innerHeight + Math.ceil(window.pageYOffset + 1)) >= document.body.offsetHeight) {
         alert("you're at the bottom of the page");
