b8421b6e21313ede85e793fbb8cad64a2aa8d042
sksynnnn <53158238+sksynnnn@users.noreply.github.com>
2022-09-17 03:09:36 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 //fetch the element
 let button = document.getElementById("button");
 
-var buttonPressed = false;
+var buttonClicked = false;
+var clickedImg = "https://cdn2.vectorstock.com/i/1000x1000/60/61/big-red-button-vector-4006061.jpg"
 
 button.addEventListener('click', event => { 
-  buttonPressed = !buttonPressed;
-  if (buttonPressed) {
+  buttonClicked = !buttonClicked;
+  if (buttonClicked) {
     beginReading();
   }
 });
 
 function beginReading() {
+  document.getElementById("button").src = clickedImg;
   alert("Begin Reading!");
 }
